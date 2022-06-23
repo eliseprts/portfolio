12bc7c08787d092fd42c35c8776ec60ae18fa452
eliseprts <elisepourtois.pro@gmail.com>
2022-06-23 14:28:58 +0200

    work section

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// Burger menu
 const menuElementsToToggle = document.querySelectorAll('.toggle-menu')
 const iconToggle = document.querySelector('.icone-toggle')
 
 const toggleMenu = () => menuElementsToToggle.forEach(el => el.classList.toggle('hidden'))
 
 iconToggle.addEventListener("click", toggleMenu)
+
+// Smooth scroll
